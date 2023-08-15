c5bafcb23e974e4309cf8633c1c7446ff069e30c
Dallin Isaacson <dallinisaacson1@gmail.com>
2023-08-15 11:42:57 -0500

    updated

--- spotify/src/app/spotify/Album.js
+++ spotify/src/app/spotify/Album.js
@@ -1,27 +1,44 @@
-// 'use client'
-// import React, {useState, useEffect} from "react"
-// import Spotify from "./Spotify"
+'use client'
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Spotify from "./Spotify"
 
-// const Album = ({Spotify}) => {
-// const [album, setAlbum] = useState()
+const Album = ({Spotify}) => {
+const [album, setAlbum] = useState()
 
-// useEffect(() => {
-//     axios.get(`'https://api.spotify.com/v1/albums/689rfQevAx4pvJnuw1MxQO'`, {
-//         headers: {
-//             Authorization: `Bearer ${token}`
-//         }
-//     })
-//     .then(function (response) {
-//       // handle success
-//       console.log(response);
-//       setAlbum(response)
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//     })   
-// }, []) 
-//     return (
-//         <Album Spotify={Spotify} />
-//     )
-// }
+// https://open.spotify.com/artist/00YTqRClk82aMchQQpYMd5?si=D-tzLJapR1iO1DEerGhybQ
+const token = 'BQAQizuc2lMyq1_gxBGHbBbe9aeXLlyODsHwi7Td4JRjoFFs0Agq1y33B9udWYpSg0Vltj4MShoqGDY0o-TONxHdMh9qr0bE7cW9eVvkYrRBWslgqjk'
+useEffect(() => {
+    axios.get('https://api.spotify.com/v1/artists/00YTqRClk82aMchQQpYMd5/albums', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      setAlbum(response)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })   
+}, []) 
+
+    return (
+        <div>
+            
+            {album.data.items.map((item) => {
+                return (
+                    <div>
+                        <div> <img  className="pic" src={item?.images?.[0]?.url}></img>     </div>
+            <h1><div className="album">{item?.name}</div> </h1>
+            <div className="artist">{item?.artists?.[0]?.name}</div> 
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Album;
--- spotify/src/app/spotify/Spotify.js
+++ spotify/src/app/spotify/Spotify.js
@@ -1,12 +1,14 @@
 'use client'
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Album from "./Album";
 
 const Spotify = () => {
 const [album, setAlbum] = useState()
 
-    const token = 'BQANudvDgkRf-MyEXTVV5AwLnqSypOvd8Aur1zk8oKBtDdM3KNGO8Ec6nQnrIg-HmiOkNfd7sTv5dj7Y9hDBYttr_guOpe6fI-0ASZyZ8aAN2yRImRk'
+    const token = 'BQAQizuc2lMyq1_gxBGHbBbe9aeXLlyODsHwi7Td4JRjoFFs0Agq1y33B9udWYpSg0Vltj4MShoqGDY0o-TONxHdMh9qr0bE7cW9eVvkYrRBWslgqjk'
 
+    
 useEffect(() => {
     axios.get('https://api.spotify.com/v1/albums/689rfQevAx4pvJnuw1MxQO', {
         headers: {
@@ -26,11 +28,9 @@ useEffect(() => {
     
 
     return (
+        
         <div>
-            
-            <div> <img  className="pic" src={album?.data?.images?.[0]?.url}></img>     </div>
-            <h1><div className="album">{album?.data?.name}</div> </h1>
-            <div className="artist">{album?.data?.artists?.[0]?.name}</div> 
+            <Album album={album} />
             
         </div>
     )
